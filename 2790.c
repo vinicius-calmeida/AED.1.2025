b7d59e8ee156e259100a0bfee433413c43b3c674
Vinícius Constantino Almeida <vinicius.constantino@unifesp.br>
2026-01-04 21:08:42 -0300

    2790.c

--- 2790.c
+++ 2790.c
@@ -1,96 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
-#define TAM_MAX_PALAVRA 21
-
-struct Item {
-    char nome[TAM_MAX_PALAVRA];
-    struct Item* prox;
-};
-
-struct Item* novoItem(char* nome);
-void insere(struct Item** ref_cabeca, char* nome);
-void imprime(struct Item* no);
-
-int main(void) {
-    int N;
-    char *linha;
-    size_t tamanho_max_linha = 1000 * (TAM_MAX_PALAVRA + 1);
+int main(){
+    int v[7], n, dados[100000], maior=0, obj=0, movimentos=0, mlhr=-1;
     
-    linha = (char*)malloc(tamanho_max_linha * sizeof(char));
-    if (linha == NULL) {
-        printf("0");
-        return 1;
+    memset(v, 0, sizeof(v));
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &dados[i]);
     }
-    struct Item* cabeca = NULL;
-    scanf("%d\n", &N);
-    for (int i = 0; i < N; i++) {
-        fgets(linha, tamanho_max_linha, stdin);
-        linha[strcspn(linha, "\n")] = 0;
-        cabeca = NULL;
-        char *token = strtok(linha, " ");
-        while (token != NULL) {
-            insere(&cabeca, token);
-            token = strtok(NULL, " ");
+
+    for (int obj = 1; obj < 8; obj++)
+    {
+        movimentos = 0;
+        for (int i = 0; i < n; i++)
+        {
+            if (dados[i] == (7-obj))
+            {
+                movimentos+=2;
+            }
+            if (dados[i] != obj && dados[i] != (7-obj))
+            {
+                movimentos+=1;
+            }
         }
-        imprime(cabeca);
-    }
-
-    free(linha);
-    return 0;
-}
-
-struct Item* novoItem(char* nome) {
-    struct Item* novoItem = (struct Item*)malloc(sizeof(struct Item));
-    if (novoItem == NULL) {
-        printf("Erro: Falha ao alocar memória para novoItem.\n");
-        exit(EXIT_FAILURE);
-    }
-    strcpy(novoItem->nome, nome);
-    novoItem->prox = NULL;
-    return novoItem;
-}
-
-void insere(struct Item** ref_cabeca, char* nome) {
-    struct Item* novo_item = novoItem(nome);
-    struct Item* anterior = NULL;
-    struct Item* atual = *ref_cabeca;
-
-    if (*ref_cabeca == NULL || strcmp(novo_item->nome, atual->nome) < 0) {
-        novo_item->prox = *ref_cabeca;
-        *ref_cabeca = novo_item;
-        return;
-    }
-
-    while (atual != NULL && strcmp(atual->nome, novo_item->nome) < 0) {
-        anterior = atual;
-        atual = atual->prox;
-    }
-
-    if (anterior == NULL) {
-        novo_item->prox = *ref_cabeca;
-        *ref_cabeca = novo_item;
-    } else {
-        anterior->prox = novo_item;
-        novo_item->prox = atual;
-    }
-}
-
-void imprime(struct Item* no) {
-    struct Item* atual = no;
-    struct Item* proximo;
-  
-    while (atual != NULL) {
-        printf("%s", atual->nome);
-        proximo = atual->prox;
-        while (proximo != NULL && strcmp(atual->nome, proximo->nome) == 0) {
-            proximo = proximo->prox;
+        if (mlhr == -1 || movimentos < mlhr)
+        {
+            mlhr = movimentos;
         }
-        atual = proximo;
-        if (atual != NULL) {
-            printf(" ");
-        }
+        
     }
-    printf("\n");
+    printf("%d\n", mlhr);
 }
