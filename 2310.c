c97067faed3762038e08228f06f7f1747d17487a
Vinícius Constantino Almeida <vinicius.constantino@unifesp.br>
2026-01-08 18:26:25 -0300

    2310.c

--- 2310.c
+++ 2310.c
@@ -3,27 +3,25 @@
 #include <stdlib.h>
 #include <math.h>
 
-
 struct Jogador{
-	int saques, bloqueios, ataques;
-	int PontoS, PontoB, PontoA;
+	int saques, bloqueios, ataques, PontoS, PontoB, PontoA;
 };
 
 int main()
 {
-	short NJogadores;
+	short Njogadores;
 	char nomeJogador[100];
 	long S = 0;
     long B = 0;
     long A = 0;
-    long TotalPontoS = 0;
-    long TotalPontoB = 0;
-    long TotalPontoA = 0;
+    long TotPontoS = 0;
+    long TotPontoB = 0;
+    long TotPontoA = 0;
 	struct Jogador jogador;
 	
-	scanf("%hd", &NJogadores);
+	scanf("%hd", &Njogadores);
 	
-	while(NJogadores--)
+	while(Njogadores--)
     {
 		scanf("%s %d %d %d", nomeJogador, &jogador.saques, &jogador.bloqueios, &jogador.ataques);
 		scanf("%d %d %d", &jogador.PontoS, &jogador.PontoB, &jogador.PontoA);
@@ -31,16 +29,13 @@ int main()
         S += jogador.saques;
         B += jogador.bloqueios;
         A += jogador.ataques;	
-
-        TotalPontoS += jogador.PontoS;
-        TotalPontoB += jogador.PontoB;
-        TotalPontoA += jogador.PontoA;
-		
+        TotPontoS += jogador.PontoS;
+        TotPontoB += jogador.PontoB;
+        TotPontoA += jogador.PontoA;
 	}
-
-	printf("Pontos de Saque: %.2lf %%.\n", (TotalPontoS*100.)/S);
-	printf("Pontos de Bloqueio: %.2lf %%.\n", (TotalPontoB*100.)/B);
-	printf("Pontos de Ataque: %.2lf %%.\n", (TotalPontoA*100.)/A);
+	printf("Pontos de Saque: %.2lf %%.\n", (TotPontoS*100.)/S);
+	printf("Pontos de Bloqueio: %.2lf %%.\n", (TotPontoB*100.)/B);
+	printf("Pontos de Ataque: %.2lf %%.\n", (TotPontoA*100.)/A);
 	
 	return 0;
 }
