e8f3c9da84e01d177a0f5ff02e79c9e891a15c0d
Vinícius Constantino Almeida <vinicius.constantino@unifesp.br>
2026-01-08 17:27:21 -0300

    1187.c

--- 1187.c
+++ 1187.c
@@ -7,28 +7,20 @@ int main() {
     int contador = 0;
 
     scanf(" %c", &O);
-
-
     for (int i = 0; i < 12; i++) {
         for (int j = 0; j < 12; j++) {
             scanf("%lf", &M[i][j]);
         }
     }
-
-
     for (int i = 0; i < 6; i++) {
         for (int j = i + 1; j < 12 - i - 1; j++) {
             resultado += M[i][j];
             contador++;
         }
     }
-
-
     if (O == 'M') {
         resultado /= contador;
     }
-
- 
     printf("%.1f\n", resultado);
 
     return 0;
